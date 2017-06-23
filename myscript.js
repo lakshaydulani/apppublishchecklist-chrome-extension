dc108a6f95231dcbb6dd72ec6017631bec31b95f
Lakshay Dulani <lakshaydulani@gmail.com>
2017-06-23 14:40:39 +0530

    basic done

--- myscript.js
+++ myscript.js
@@ -1,5 +1,97 @@
-document.querySelector("input[type='submit']").style.color='red';
-document.querySelector("input[type='submit']").onclick = function(){
-    alert('locked');
-    return false;
+
+var elem, popup;
+window.addEventListener("hashchange", applyOnPage, false);
+
+
+function applyOnPage() {
+    if (window.location.hash.indexOf("PrepareReleasePlace") != -1)
+        restrictButton();
+}
+
+function restrictButton() {
+    elem = null;
+    for (var i = 0; i < document.querySelectorAll("div.GULSEEC-f-m").length; i++) { if (document.querySelectorAll("div.GULSEEC-f-m")[i].innerText == 'REVIEW') { elem = document.querySelectorAll("div.GULSEEC-f-m")[i].parentNode; } }
+    if (elem) {
+        //elem.disabled = true;
+        var but = document.createElement("button");
+        but.id = "extension-main-button";
+        but.innerText = 'Review';
+        but.className = 'GULSEEC-f-a GULSEEC-f-r';
+        but.onclick = constructShowPopup;
+        elem.parentNode.append(but);
+        elem.style.display = 'none';
+    }
+}
+
+function constructShowPopup() {
+    popup = document.createElement("div");
+    popup.id = "extension-popup";
+    popup.style.position = 'fixed';
+    popup.style.width = '200px';
+    popup.style.height = '200px';
+    popup.style.zIndex = 9999;
+    popup.style.top = 'calc(50% - 100px)';
+    popup.style.left = 'calc(50% - 100px)';
+    popup.style.border = '2px solid black';
+    popup.style.backgroundColor = 'white';
+    document.body.append(popup);
+
+    var ul = document.createElement('ul');
+    ul.id = 'extension-taskList';
+
+    ul.innerHTML = '';
+
+    var taskList = [{ name: 'test 1 ' }, { name: 'test 2 ' }];
+
+    taskList.forEach(function (task) {
+        var listItem = document.createElement('li'),
+            taskLabel = document.createElement('label'),
+            chk = document.createElement('input');
+
+        chk.className = 'task-Chk';
+        chk.type = 'checkbox';
+        chk.onchange = unlockReview;
+
+        listItem.className = 'task';
+        listItem.id = taskList.indexOf(task);
+
+        taskLabel.className = 'taskLabel';
+        taskLabel.textContent = task.name;
+        taskLabel.htmlFor = 'c' + taskList.indexOf(task);
+
+        listItem.appendChild(chk);
+        listItem.appendChild(taskLabel);
+        ul.appendChild(listItem);
+    });
+
+    popup.append(ul);
+    popup.style.display = 'block';
+
+}
+
+function unlockReview() {
+
+    var listDone = true;
+
+    for (var i = 0; i < document.querySelectorAll(".task-Chk").length; i++) {
+        if (document.querySelectorAll(".task-Chk")[i].checked == false) {
+            listDone = false;
+            break;
+        }
+    }
+
+    if (listDone && elem) {
+
+
+        popup.style.display = 'none';
+        elem.style.display = 'block';
+        elem.click();
+
+
+    }
+    else {
+        console.log('couldnt proceed', elem, popup);
+    }
+
+
 }
--- options.js
+++ options.js
@@ -91,52 +91,8 @@ function saveLocalList() {
 	chrome.storage.sync.set({
 		items: JSON.stringify(taskList)
 	}, function () {
-		// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function () {
-			status.textContent = '';
-		}, 750);
+		
 	});
 
 	//localStorage.setItem("taskList", JSON.stringify( taskList ));
 }
-
-// function save_options() {
-//   var color = document.getElementById('color').value;
-//   var likesColor = document.getElementById('like').checked;
-//   chrome.storage.sync.set({
-//     favoriteColor: color,
-//     likesColor: likesColor
-//   }, function() {
-//     // Update status to let user know options were saved.
-//     var status = document.getElementById('status');
-//     status.textContent = 'Options saved.';
-//     setTimeout(function() {
-//       status.textContent = '';
-//     }, 750);
-//   });
-// }
-
-// // Restores select box and checkbox state using the preferences
-// // stored in chrome.storage.
-// function restore_options() {
-//   // Use default value color = 'red' and likesColor = true.
-//   chrome.storage.sync.get({
-//     favoriteColor: 'red',
-//     likesColor: true
-//   }, function(items) {
-//    var list = items.list;
-
-//    for(var i=0; i< list.length; i++){
-
-
-
-
-//    }
-
-//   });
-// }
-// document.addEventListener('DOMContentLoaded', restore_options);
-// document.getElementById('save').addEventListener('click',
-//     save_options);
